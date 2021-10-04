9c190fb751aa549a8dbcb81087043a384619b685
Karl Taguibao <karltag2018@gmail.com>
2021-10-04 21:15:59 +0800

    fixed images v2

--- views/Projects.js
+++ views/Projects.js
@@ -18,14 +18,14 @@ export default class extends Abstract {
             <div class="bottom-portion">
               <div class="projects-container">
                 <div class="image-container one">
-                    <a href="https://foncemere.github.io/ClockWebsite/"><span class="go">Go to site <img src="./assets/arrow_forward_white_24dp.svg"></span></a>
-                    <a href="https://github.com/Foncemere/ClockWebsite"><span class="go-github"><img class="github-image" src="./assets/Github.png"></span></a>
+                    <a href="https://foncemere.github.io/ClockWebsite/"><span class="go">Go to site <img src="./assets/arrow.svg"></span></a>
+                    <a href="https://github.com/Foncemere/ClockWebsite"><span class="go-github"><img class="github-image" src="./assets/GitHub.png"></span></a>
                 </div>
                 <div class="image-container two">
                 <a href="https://github.com/Foncemere/MessagingProject"><span class="go-github"><img class="github-image" src="./assets/GitHub.png"></span></a>
                 </div>
                 <div class="image-container three">
-                    <a href="https://github.com/Foncemere/toDoReact/"><span class="go">Go to site <img src="/assets/arrow_forward_white_24dp.svg"></span></a>
+                    <a href="https://github.com/Foncemere/toDoReact/"><span class="go">Go to site <img src="./assets/arrow.svg"></span></a>
                     <a href="https://github.com/Foncemere/toDoReact"><span class="go-github"><img class="github-image" src="./assets/GitHub.png"></span></a>
                 </div>
               </div>
